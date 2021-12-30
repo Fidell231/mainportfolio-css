72863af2d1f5b213cf5a1677974c41d33827ae99
Fidell231 <fidelliskimathi@gmail.com>
2021-12-30 17:41:59 +0300

    Fidelliskimathi portfolio

---
+++ app.js
@@ -0,0 +1,119 @@
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
+const navLogo = document.querySelector("#navbar__logo");
+
+// Display Mobile Menu
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+
+// Show active menu when scrolling
+const highlightMenu = () => {
+  const elem = document.querySelector(".highlight");
+  const homeMenu = document.querySelector("#home-page");
+  const aboutMenu = document.querySelector("#about-page");
+  const servicesMenu = document.querySelector("#services-page");
+  let scrollPos = window.scrollY;
+  // console.log(scrollPos);
+
+  // adds 'highlight' class to my menu items
+  if (window.innerWidth > 960 && scrollPos < 600) {
+    homeMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    aboutMenu.classList.add("highlight");
+    homeMenu.classList.remove("highlight");
+    servicesMenu.classList.remove("highlight");
+    return;
+  } else if (window.innerWidth > 960 && scrollPos < 2345) {
+    servicesMenu.classList.add("highlight");
+    aboutMenu.classList.remove("highlight");
+    return;
+  }
+
+  if ((elem && window.innerWIdth < 960 && scrollPos < 600) || elem) {
+    elem.classList.remove("highlight");
+  }
+};
+
+window.addEventListener("scroll", highlightMenu);
+window.addEventListener("click", highlightMenu);
+
+//  Close mobile Menu when clicking on a menu item
+const hideMobileMenu = () => {
+  const menuBars = document.querySelector(".is-active");
+  if (window.innerWidth <= 768 && menuBars) {
+    menu.classList.toggle("is-active");
+    menuLinks.classList.remove("active");
+  }
+};
+
+menuLinks.addEventListener("click", hideMobileMenu);
+navLogo.addEventListener("click", hideMobileMenu);
+
+// List of sentences
+var _CONTENT = [
+  "Quality is not act. It is habit",
+  "Quality is never an accident, it is the result of skill",
+  "Everything you imagine is real",
+  "Creativity is intelligence having fun",
+  "Indivudualy we are a drop, together we are a ocean",
+  "If you want to lift yourself up, lift up someone else",
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Implements typing effect
+function Type() {
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX++;
+
+  // If full sentence has been displayed then start to delete the sentence after some time
+  if (text === _CONTENT[_PART]) {
+    clearInterval(_INTERVAL_VAL);
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(Delete, 50);
+    }, 1000);
+  }
+}
+
+// Implements deleting effect
+function Delete() {
+  var text = _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+  _ELEMENT.innerHTML = text;
+  _PART_INDEX--;
+
+  // If sentence has been deleted then start to display the next sentence
+  if (text === "") {
+    clearInterval(_INTERVAL_VAL);
+
+    // If last sentence then display the first one, else move to the next
+    if (_PART == _CONTENT.length - 1) _PART = 0;
+    else _PART++;
+    _PART_INDEX = 0;
+
+    // Start to display the next sentence after some time
+    setTimeout(function () {
+      _INTERVAL_VAL = setInterval(Type, 100);
+    }, 200);
+  }
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
+
